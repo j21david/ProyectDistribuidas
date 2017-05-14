8d36a3b21cfaed78f76060fc6cea4f382e14a4a9
Jose Gonzalez <jose21gonzalez@gmail.com>
2017-05-13 20:40:08 -0500

    Objeto Quimico para ordenar las listas

--- src/com/epn/Main.java
+++ src/com/epn/Main.java
@@ -5,6 +5,10 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class Main {
 	public static float matriz[][]=new float[12384][12384];
@@ -37,17 +41,34 @@ public class Main {
 		
 		Redactor c = new Redactor();
 		Lector l = new Lector();
+		ArrayList<Quimico> arrayList = new ArrayList<Quimico>();
 		 
 		for(int i=0;i<40;i++){
 			for(int j=0;j<40;j++){
 				
 				//System.out.print(String.format("%.2f ",calc.result()[i][j]));
 				if(matriz[i][j]>0.5){
-				c.escribir(l.Ids[i],l.Ids[j],String.format("%.2f ",matriz[i][j]));
+					 Quimico  k= new Quimico();
+					k.setCompuesto1(l.Ids[i]);
+					k.setCompuesto2(l.Ids[j]);
+					k.setResultado(matriz[i][j]);
+					arrayList.add(k);
+					
+				//c.escribir(l.Ids[i],l.Ids[j],String.format("%.2f ",matriz[i][j]));
 				}
 			}
 		}
+		Collections.sort(arrayList);
 		
+		Iterator itListaempleado=arrayList.iterator();
+        
+        //imprime la lista sin ordenar
+        while (itListaempleado.hasNext()) {
+            Quimico elementoLista=(Quimico)itListaempleado.next();
+
+            c.escribir(elementoLista.getCompuesto1(),elementoLista.getCompuesto2(),String.format("%.2f ",elementoLista.getResultado()));
+        }
+	
 		time_end = System.currentTimeMillis()/1000;
 		System.out.println("the task has taken "+ ( time_end - time_start ) +" seconds");
 
---
+++ src/com/epn/Quimico.java
@@ -0,0 +1,47 @@
+package com.epn;
+
+public class Quimico implements Comparable<Quimico>{
+	
+	String Compuesto1=" ";
+	String Compuesto2=" ";
+	float resultado;
+	
+	
+	public String getCompuesto1() {
+		return Compuesto1;
+	}
+	public void setCompuesto1(String compuesto1) {
+		Compuesto1 = compuesto1;
+	}
+	public String getCompuesto2() {
+		return Compuesto2;
+	}
+	public void setCompuesto2(String compuesto2) {
+		Compuesto2 = compuesto2;
+	}
+	public float getResultado() {
+		return resultado;
+	}
+	public void setResultado(float resultado) {
+		this.resultado = resultado;
+	}
+	@Override
+	public int compareTo(Quimico o) {
+		 int byAge = Float.compare(o.resultado, resultado);
+	        if ( byAge != 0 ) {
+	            return byAge;
+	        }
+	        if ( Compuesto1 == null ) {
+	            return o.Compuesto1 == null ? 0 : 1;
+	        }
+	        if ( o.Compuesto1 == null ) {
+	            return 1;
+	        }
+	        return Compuesto1.compareToIgnoreCase(o.Compuesto1);
+	}
+	
+	
+	
+	
+
+}
